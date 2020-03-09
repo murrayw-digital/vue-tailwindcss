d90895dd24cf5ad478cd8fb5a6e3a10c7f495eb3
Murray Williams <hello@murraywilliams.co.za>
2020-03-09 18:33:36 +0200

    Setup Tailwind css boilerplate with vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen w-screen bg-blue-600 flex justify-center content-center flex-wrap">
+    <h1 class="text-5xl text-blue-100 font-bold text-white">Hello from Tailwind</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style>
+</style>
